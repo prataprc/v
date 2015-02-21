dd6746756977e0a6163450a92634de1a712b69be
prataprc <prataprc@gmail.com>
2015-02-21 11:20:07 +0530

    Added algorithms to converts buffer into lines.

--- buffer/lines.go
+++ buffer/lines.go
@@ -2,92 +2,6 @@ package buffer
 
 import "fmt"
 
-// BuildBlock around specified cursor position,
-// if `bCur` is -1 use current cursor position.
-// Return Lines of specified width*2 + 1.
-func (ebuf *EditBuffer) BuildBlock(bCur int64, width int64) Lines {
-	if bCur < 0 {
-		bCur = ebuf.dot
-	}
-	lnNl := int64(len(ebuf.newline))
-	lines := make(Lines, 0, (width*2+1)*2+2)
-	// gather lines above cursor.
-	iter := Find(ebuf.reNlR, ebuf.buffer.BackStreamFrom(bCur))
-	for i, end := int64(0), int64(-1); i < width+1; i++ {
-		loc := iter()
-		if loc != nil {
-			lines = append(lines, end, int64(loc[0])+lnNl)
-			end = int64(loc[1])
-		} else {
-			lines = append(lines, end, 0)
-			break
-		}
-	}
-	lines.reverse()
-	// gather lines below cursor.
-	iter = Find(ebuf.reNlR, ebuf.buffer.StreamFrom(bCur))
-	for i, start := int64(0), int64(-1); i < width; i++ {
-		loc := iter()
-		if loc != nil {
-			lines = append(lines, start, int64(loc[1]))
-			start = int64(loc[1])
-		} else {
-			l, err := ebuf.buffer.Length()
-			if err != nil {
-				err = fmt.Errorf("impossible situation: %v\n", err)
-				panic(err)
-			}
-			lines = append(lines, start, l)
-			break
-		}
-	}
-	// consolidate.
-	for i, j := 1, 1; (i + 1) < len(lines); i += 2 {
-		if lines[i] != lines[i+1] {
-			panic("impossible situation")
-		} else if lines[i] == -1 && lines[i+1] == -1 {
-			continue
-		}
-		lines[j], lines[j+1] = lines[i], lines[i+1]
-		j += 2
-	}
-	return lines
-}
-
-// Line returns the line starting from `start` and ending
-// before `end`.
-func (ebuf *EditBuffer) Line(bCur int64) (start int64, end int64) {
-	i := ebuf.lines.indexof(bCur)
-	if i == -1 {
-		return -1, -1
-	}
-	start, end = ebuf.lines[i], ebuf.lines[i+1]
-	return start, end
-}
-
-// Lines returns a block of Lines around the line containing
-// bCur.
-func (ebuf *EditBuffer) Lines(bCur int64, width int64) (ls Lines) {
-	idx := ebuf.lines.indexof(bCur)
-	if idx == -1 {
-		ebuf.lines = ebuf.lines.merge(ebuf.BuildBlock(bCur, width))
-		idx = ebuf.lines.indexof(bCur)
-	}
-	if idx == -1 {
-		panic(fmt.Errorf("impossible situation\n"))
-	}
-	start := idx - width*2
-	if start < 0 {
-		start = 0
-	}
-	n := (width*2 + 1) * 2
-	lines := make(Lines, 0, n)
-	for i, j := start, int64(0); j < n; i, j = i+2, j+2 {
-		lines[j], lines[j+1] = ebuf.lines[j], ebuf.lines[i+1]
-	}
-	return lines
-}
-
 // Lines index pairs within the input buffer,
 // eg lines[2*n:2*n+1] identifies the indexes of the
 // nth line starting from lines[2*n] and ending
@@ -103,56 +17,179 @@ func (ebuf *EditBuffer) Lines(bCur int64, width int64) (ls Lines) {
 //      [..., x, n+1]
 type Lines []int64
 
-func (lines Lines) indexof(bCur int64) int64 {
-	if len(lines)%2 != 0 {
-		panic(fmt.Errorf("impossible situation\n"))
-	}
-	for i := 0; i <= len(lines); i += 2 {
-		x, y := lines[i], lines[i+1]
-		if x < 0 {
-			panic(fmt.Errorf("impossible situation\n"))
-		} else if y < 0 {
-			panic(fmt.Errorf("impossible situation\n"))
-		} else if x > y {
-			panic(fmt.Errorf("impossible situation\n"))
-		} else if bCur >= x && bCur < y {
-			return int64(i)
-		} else if bCur < x {
-			return -1
-		}
-	}
-	return -1
+// LinesIterator will iterate on blocks of lines
+// within edit-buffer.
+type LinesIterator func() Lines
+
+// LineIterator will iterate on each consecutive line
+// within a block.
+type LineIterator func() (start, end int64)
+
+// return an iterator on block of consecutive-lines.
+func (lines Lines) blocks() LinesIterator {
+    lines.checkSanity(false)
+    i := 0
+    return LinesIterator(func() Lines {
+        block := make(Lines, 0, 16)
+        for i < len(lines) {
+            start, end := lines[i], lines[i+1]
+            block = append(block, start, end)
+            i += 2
+            if i < len(lines)  &&  end != lines[i] {
+                return block
+            }
+        }
+        if len(block) > 0 {
+            return block
+        }
+        return nil
+    })
 }
 
+// return an iterator on block of consecutive-lines,
+// first block will contain bCur.
+func (lines Lines) blocksFrom(bCur int64) LinesIterator {
+    lines.checkSanity(false)
+    iterBlock := lines.blocks()
+    return LinesIterator(func() Lines {
+        for {
+            block := iterBlock()
+            if bCur < 0 {
+                return block
+            } else if block != nil  {
+                if block.containsCursor(bCur) || block.afterCursor(bCur) {
+                    bCur = -1 // let us iterate on each block from now on.
+                    return block
+                }
+            }
+            // NOTE: continue to find the next block containing bCur.
+        }
+    })
+}
+
+// Lines will return an iterator for consecutive lines.
+func (lines Lines) Lines(bCur int64) LineIterator {
+    lines.checkSanity(false)
+    from := 0
+    if bCur > 0 {
+        for ; from < len(lines); from += 2 {
+            if (bCur >= lines[from]  &&  bCur < lines[from+1]) ||
+               (bCur < lines[from]) {
+                break
+            }
+        }
+    }
+    return LineIterator(func() (start, end int64) {
+        prev := from
+        if from > 0  &&  from < len(lines) {
+            from += 2
+            start, end := lines[prev], lines[prev+1]
+            if from >= len(lines)  ||  lines[prev+1] != lines[from] {
+                from = -1
+            }
+            return start, end
+        }
+        return -1, -1
+    })
+}
+
+// merge a consecutive block of line, into lines.
+func (lines Lines) mergeBlock(block Lines) Lines {
+    block.checkSanity(true); lines.checkSanity(false)
+    if len(block) == 0 {
+        return lines
+    }
+    i, floorIdx, remaining := 0, 0, []int64{}
+    a := block[0]
+    for ; i < len(lines); i += 2 {
+        if lines[i+1] >= a {
+            z := block[len(block)-1]
+            for i = i+2; i < len(lines); i += 2 {
+                if lines[i] >= z {
+                    remaining = lines[i:]
+                    break
+                }
+            }
+            lines = lines[:floorIdx+2]
+            break
+        }
+        floorIdx = i
+    }
+    lines = append(lines, block...)
+    lines = append(lines, remaining...)
+    return lines
+}
+
+// return line index containing bCur, if the line that
+// ought to contain bCur is missing, `ok` is false, return
+// the index of first available line after bCur.
+func (lines Lines) indexof(bCur int64) (i int64, ok bool) {
+    for i := 0; i < len(lines); i += 2 {
+        x, y := lines[i], lines[i+1]
+        if x < 0 {
+            panic(fmt.Errorf("impossible situation\n"))
+        } else if y < 0 {
+            panic(fmt.Errorf("impossible situation\n"))
+        } else if x > y {
+            panic(fmt.Errorf("impossible situation\n"))
+        } else if bCur >= x && bCur < y {
+            return int64(i), true
+        } else if bCur < x {
+            return int64(i), false
+        }
+    }
+    return -1, false
+}
+
+// assuming that lines are contiguous, check wether bCur falls
+// within the lines covered.
+func (lines Lines) containsCursor(bCur int64) bool {
+    lines.checkSanity(true)
+    if bCur < 0 {
+        return true
+    } else if bCur >= lines[0]  &&  bCur < lines[len(lines)-1] {
+        return true
+    }
+    return false
+}
+
+// return whether the lines start after the cursor.
+func (lines Lines) afterCursor(bCur int64) bool {
+    if len(lines) > 0 {
+        return bCur < lines[0]
+    }
+    return false
+}
+
+// [ 40, 30, 30, 20, 20, 10 ] -> [ 10, 20, 20, 30, 30, 40 ]
 func (lines Lines) reverse() {
-	n := len(lines)
-	for i, j := 0, n-1; i < (n / 2); i, j = i+1, j-1 {
-		lines[j], lines[i] = lines[i], lines[j]
-	}
+    n := len(lines)
+    for i, j := 0, n-1; i < (n / 2); i, j = i+1, j-1 {
+        lines[j], lines[i] = lines[i], lines[j]
+    }
 }
 
-func (lines Lines) merge(ls Lines) Lines {
-	if len(ls)%2 != 0 {
-		panic(fmt.Errorf("impossible situation\n"))
-	} else if len(ls) == 0 {
-		return lines
-	}
-	i, floorIdx, remaining := 0, 0, []int64{}
-	for ; i < len(lines); i += 2 {
-		if ls[0] < lines[i+1] {
-			z := ls[len(ls)-1]
-			for ; i < len(lines); i += 2 {
-				if z <= lines[i] {
-					remaining = lines[i:]
-					break
-				}
-			}
-			lines = lines[:floorIdx]
-			break
-		}
-		floorIdx = i
-	}
-	lines = append(lines, ls...)
-	lines = append(lines, remaining...)
-	return lines
+// [ 30, 40, 20, 30, 10, 20 ] -> [ 10, 20, 20, 30, 30, 40 ]
+func (lines Lines) flipReverse() {
+    n := len(lines)
+    for i, j := 0, n-1; i < (n / 2); i, j = i+2, j-2 {
+        lines[j-1], lines[i] = lines[i], lines[j-1]
+        lines[j], lines[i+1] = lines[i+1], lines[j]
+    }
+}
+
+// check for various sanity of lines.
+func (lines Lines) checkSanity(block bool) {
+    if (len(lines) % 2) != 0 {
+        panic(fmt.Errorf("impossible situation\n"))
+    }
+    if block && len(lines) > 0 {
+        end := lines[1]
+        for i := 2; i < len(lines); i+=2 {
+            if end != lines[i] {
+                panic(fmt.Errorf("lines are not contiguous\n"))
+            }
+            end = lines[i+1]
+        }
+    }
 }
